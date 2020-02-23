3959250e119a377dbe014c071fc929464e1e0b4c
Connor <cjl1053@wildcats.unh.edu>
2020-02-22 20:29:42 -0500

    Add server - database connection and python file to clear database

--- RidePlanner/server.py
+++ RidePlanner/server.py
@@ -1,6 +1,12 @@
 from http.server import *
 import json
 
+import db
+
+
+def get_driver_route(name):
+    return f"Driver route for {name}".encode()
+
 
 class RideShareRequestHandler(BaseHTTPRequestHandler):
     def _set_headers(self):
@@ -12,7 +18,7 @@ class RideShareRequestHandler(BaseHTTPRequestHandler):
         self._set_headers()
         uri_parts = self.path[1:].split('/')
         if uri_parts[0] == "drivers":
-            self.wfile.write(self.get_driver_route(uri_parts[1]))
+            self.wfile.write(get_driver_route(uri_parts[1]))
         elif uri_parts[1] == "passengers":
             self.wfile.write(self.get_passenger_assignment(uri_parts[1]))
 
@@ -28,17 +34,19 @@ class RideShareRequestHandler(BaseHTTPRequestHandler):
         elif uri_parts[1] == "passenger":
             self.register_passenger(body)
 
-    def get_driver_route(self, name):
-        return f"Driver route for {name}".encode()
-
-    def get_passenger_assignment(self, name):
+    @staticmethod
+    def get_passenger_assignment(name):
         return f"Passenger assignment for {name}".encode()
 
-    def register_driver(self, driver):
+    @staticmethod
+    def register_driver(driver):
         print("Register new driver...")
+        db.add_driver(driver)
 
-    def register_passenger(self, passenger):
+    @staticmethod
+    def register_passenger(passenger):
         print("Register new passenger...")
+        db.add_passenger(passenger)
 
 
 def run(server_class=HTTPServer, handler_class=RideShareRequestHandler):
