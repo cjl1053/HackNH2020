033fd6962e45455ac53083567981f783e3d8b2b0
Connor <cjl1053@wildcats.unh.edu>
2020-02-22 19:55:46 -0500

    Updated Passenger object

--- RideShare/app/src/main/java/com/team6/rideshare/data/Passenger.java
+++ RideShare/app/src/main/java/com/team6/rideshare/data/Passenger.java
@@ -2,33 +2,49 @@ package com.team6.rideshare.data;
 
 import android.location.Address;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public class Passenger {
 
-    private Address mAddress;
-    private Address mPollingLocation;
-    private int mAmount;
-    private String mName;
+    private Address address;
+    private Address pollingLocation;
+    private int amount;
+    private String name;
+    private int startTime;
+    private int endTime;
 
-    public Passenger(Address address, Address pollingLocation, String name, int amount) {
-        mAddress = address;
-        mPollingLocation = pollingLocation;
-        mAmount = amount;
-        mName = name;
+    public Passenger(Address address, Address pollingLocation, String name, int amount,
+                     int startTime, int endTime) {
+        this.address = address;
+        this.pollingLocation = pollingLocation;
+        this.amount = amount;
+        this.name = name;
+        this.startTime = startTime;
+        this.endTime = endTime;
     }
 
+    @JsonIgnore
     public Address getAddress() {
-        return mAddress;
+        return address;
     }
 
     public Address getPollingLocation() {
-        return mPollingLocation;
+        return pollingLocation;
     }
 
     public int getAmount() {
-        return mAmount;
+        return amount;
     }
 
     public String getName() {
-        return mName;
+        return name;
+    }
+
+    public int getStartTime() {
+        return startTime;
+    }
+
+    public int getEndTime() {
+        return endTime;
     }
 }
